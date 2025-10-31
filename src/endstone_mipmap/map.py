d74ef121539f7f5879925ce45b63c38ca34196e7
Mipa <toti.3211@mail.ru>
2025-11-01 01:48:32 +0300

    Add chunk mapping plugin with HTTP API

--- src/endstone_mipmap/map.py
+++ src/endstone_mipmap/map.py
@@ -1,14 +1,64 @@
+import requests
+import multiprocessing as mp
+
 from endstone.plugin import Plugin
+from endstone.event import event_handler, ChunkLoadEvent
+
+
+def sendChunkData(queue: mp.Queue, config: dict) -> None:
+    while True:
+        chunkData = queue.get()
+        try:
+            requests.post(config.get("mapUrl"), json=chunkData, timeout=5)
+        except Exception as e:
+            print(f"Error sending chunk: \n{e}")
 
 
 class Map(Plugin):
     api_version = "0.10"
 
-    def on_load(self) -> None:
-        self.logger.info("on_load is called!")
-
     def on_enable(self) -> None:
-        self.logger.info("on_enable is called!")
+        self.save_default_config()
+        self.register_events(self)
+        
+        self._chunksQueue = mp.Queue()
 
-    def on_disable(self) -> None:
-        self.logger.info("on_disable is called!")
+        self._chunkDataSenderProcess = mp.Process(target=sendChunkData, args=(self._chunksQueue, self.config))
+        self._chunkDataSenderProcess.start()
+
+    @event_handler
+    def loadChunk(self, event: ChunkLoadEvent):
+        chunkData = self._getСhunkData(event)
+        
+        self._chunksQueue.put(chunkData)
+
+    def _getСhunkData(self, event: ChunkLoadEvent) -> dict:
+        world = event.chunk.dimension
+        chunkX = event.chunk.x
+        chunkZ = event.chunk.z
+        
+        chunkStartX = chunkX * 16
+        chunkStartZ = chunkZ * 16
+        chunkEndX = chunkStartX + 16
+        chunkEndZ = chunkStartZ + 16
+
+        blocksData = []
+
+        for x in range(chunkStartX, chunkEndX):
+            for z in range(chunkStartZ, chunkEndZ):                
+                block = world.get_highest_block_at(x, z)
+                blockType = block.data.type
+
+                blocksData.append({
+                    "name": blockType,
+                    "coordinates": [block.x, block.y, block.z]
+                })
+        
+        chunkData = {
+            "chunk": {
+                "dimension": world.name,
+                "blocks": blocksData
+            }
+        }
+
+        return chunkData
